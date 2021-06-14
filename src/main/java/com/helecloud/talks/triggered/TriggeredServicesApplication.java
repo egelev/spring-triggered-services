fa6d270a8e8de1818aa02a1e05215add3006ee6d
Emil Gelev <emil.gelev@ft.com>
2021-06-14 19:00:18 +0300

    Initial commit from Spring Initilizer

---
+++ src/main/java/com/helecloud/talks/triggered/TriggeredServicesApplication.java
@@ -0,0 +1,13 @@
+package com.helecloud.talks.triggered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriggeredServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriggeredServicesApplication.class, args);
+	}
+
+}
